62c3cf2039d0d38d7af8053cf72c24931166511a
Sony Arianto Kurniawan <sony@sony-ak.com>
2023-03-21 00:21:08 +0700

    Update version to 0.1.2 in package.json and config.ts files

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,7 +1,7 @@
 import path from "path";
 
 export const APP_NAME = "sshmgr";
-export const APP_VERSION = "0.1.1";
+export const APP_VERSION = "0.1.2";
 export const CONFIG_FILE_NAME = ".sshmgr.config.json";
 export const CONFIG_FILE_PATH = path.join(
   process.env.HOME || process.env.USERPROFILE || "",
