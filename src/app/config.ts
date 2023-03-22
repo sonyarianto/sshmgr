8e276d553114423d686a6edfd80150f625f2a7dc
Sony Arianto Kurniawan <sony@sony-ak.com>
2023-03-22 13:32:07 +0700

    Update APP_VERSION to dynamically reference package.json version in config.ts file

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,7 +1,8 @@
 import path from "path";
+import packageJson from "../../package.json";
 
 export const APP_NAME = "sshmgr";
-export const APP_VERSION = "0.1.6";
+export const APP_VERSION = packageJson.version;
 export const CONFIG_FILE_NAME = ".sshmgr.config.json";
 export const CONFIG_FILE_PATH = path.join(
   process.env.HOME || process.env.USERPROFILE || "",
