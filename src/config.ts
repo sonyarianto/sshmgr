496b2bf909dbc4a858e9a36d675c894917783215
Sony Arianto Kurniawan <sony@sony-ak.com>
2023-04-02 20:25:14 +0700

    config.ts: update

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import path from "path";
 import packageJson from "../package.json";
 
-export const APP_NAME = "sshmgr";
+export const APP_NAME = packageJson.name;
 export const APP_VERSION = packageJson.version;
 export const CONFIG_FILE_NAME = ".sshmgr.config.json";
 export const CONFIG_FILE_PATH = path.join(
